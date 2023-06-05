fcf72812f86a7af1e3ada18ae3b08683df343534
admatysz <admatysz@assecobs.pl>
2023-06-05 19:29:53 +0200

    cofnięcie opisu

--- src/main/java/com/example/friendly_fishstick/OpenAPIConfiguration.java
+++ src/main/java/com/example/friendly_fishstick/OpenAPIConfiguration.java
@@ -18,7 +18,7 @@ import org.springframework.context.annotation.Configuration;
         
            customer: {customer:password}""")
 @OpenAPIDefinition(
-        info = @Info(title = "Shop API", version = "v1", description = "Make sure to authorize by clicking 'Authorize' button on right"),
+        info = @Info(title = "Shop API", version = "v1"),
         security = @SecurityRequirement(name = "basicAuth")
 )
 public class OpenAPIConfiguration {
